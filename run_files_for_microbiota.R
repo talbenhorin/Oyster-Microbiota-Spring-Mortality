a03ab565e902360bd141079b723964bd2e760faf
Tal Ben-Horin PhD <tbenhor@wolftech.ad.ncsu.edu>
2020-09-15 11:02:49 -0400

    update

--- run_files_for_microbiota.R
+++ run_files_for_microbiota.R
@@ -1,3 +1,31 @@
 rm(list=ls(all=TRUE))
 
-BiocManager::install("MassSpecWavelet")
+## Install dada2 package
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+BiocManager::install("dada2", version = "3.11")
+
+library(dada2)
+
+pathF <- "E:/tbenhor/libraries/Documents/Oyster-Microbiota-Spring-Mortality/forward" # CHANGE ME to the directory containing your demultiplexed forward-read fastqs
+pathR <- "E:/tbenhor/libraries/Documents/Oyster-Microbiota-Spring-Mortality/reverse" # CHANGE ME to the directory containing your demultiplexed reverse-read fastqs
+
+filtpathF <- file.path(pathF, "filtered") # Filtered forward files go into the pathF/filtered/ subdirectory
+filtpathR <- file.path(pathR, "filtered") 
+
+fastqFs <- sort(list.files(pathF, pattern="fastq.gz"))
+fastqRs <- sort(list.files(pathR, pattern="fastq.gz"))
+
+if(length(fastqFs) != length(fastqRs)) stop("Forward and reverse files do not match.")
+
+filterAndTrim(fwd=file.path(pathF, fastqFs), filt=file.path(filtpathF, fastqFs),
+              rev=file.path(pathR, fastqRs), filt.rev=file.path(filtpathR, fastqRs),
+              truncLen=c(240,200), maxEE=2, truncQ=11, maxN=0, rm.phix=TRUE,
+              compress=TRUE, verbose=TRUE, multithread=TRUE)
+
+# Infer sequence variants
+filtFs <- list.files(filtpathF, pattern="fastq.gz", full.names = TRUE)
+filtRs <- list.files(filtpathR, pattern="fastq.gz", full.names = TRUE)
+sample.names <- sapply(strsplit(basename(filtFs), "_"), `[`, 1) # Assumes filename = samplename_XXX.fastq.gz
+sample.namesR <- sapply(strsplit(basename(filtRs), "_"), `[`, 1) # Assumes filename = samplename_XXX.fastq.gz
+if(!identical(sample.names, sample.namesR)) stop("Forward and reverse files do not match.")
