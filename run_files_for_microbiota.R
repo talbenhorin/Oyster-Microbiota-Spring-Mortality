85633df71cf11e07037ba9c2db3e2c54b9491bec
Tal Ben-Horin PhD <tbenhor@wolftech.ad.ncsu.edu>
2020-09-15 12:17:42 -0400

    update

--- run_files_for_microbiota.R
+++ run_files_for_microbiota.R
@@ -5,8 +5,9 @@ if (!requireNamespace("BiocManager", quietly = TRUE))
   install.packages("BiocManager")
 BiocManager::install("dada2", version = "3.11")
 
-library(dada2)
+library(dada2); packageVersion("dada2")
 
+## Filtering sequences
 pathF <- "E:/tbenhor/libraries/Documents/Oyster-Microbiota-Spring-Mortality/forward" # CHANGE ME to the directory containing your demultiplexed forward-read fastqs
 pathR <- "E:/tbenhor/libraries/Documents/Oyster-Microbiota-Spring-Mortality/reverse" # CHANGE ME to the directory containing your demultiplexed reverse-read fastqs
 
@@ -18,14 +19,45 @@ fastqRs <- sort(list.files(pathR, pattern="fastq.gz"))
 
 if(length(fastqFs) != length(fastqRs)) stop("Forward and reverse files do not match.")
 
+# Filtering: consider modifying these parameters if issues arise
 filterAndTrim(fwd=file.path(pathF, fastqFs), filt=file.path(filtpathF, fastqFs),
               rev=file.path(pathR, fastqRs), filt.rev=file.path(filtpathR, fastqRs),
               truncLen=c(240,200), maxEE=2, truncQ=11, maxN=0, rm.phix=TRUE,
               compress=TRUE, verbose=TRUE, multithread=TRUE)
 
 # Infer sequence variants
+# File parsing
+filtpathF <- "E:/tbenhor/libraries/Documents/Oyster-Microbiota-Spring-Mortality/forward/filtered" # CHANGE ME to the directory containing your filtered forward fastqs
+filtpathR <- "E:/tbenhor/libraries/Documents/Oyster-Microbiota-Spring-Mortality/reverse/filtered"
 filtFs <- list.files(filtpathF, pattern="fastq.gz", full.names = TRUE)
 filtRs <- list.files(filtpathR, pattern="fastq.gz", full.names = TRUE)
 sample.names <- sapply(strsplit(basename(filtFs), "_"), `[`, 1) # Assumes filename = samplename_XXX.fastq.gz
 sample.namesR <- sapply(strsplit(basename(filtRs), "_"), `[`, 1) # Assumes filename = samplename_XXX.fastq.gz
 if(!identical(sample.names, sample.namesR)) stop("Forward and reverse files do not match.")
+names(filtFs) <- sample.names
+names(filtRs) <- sample.names
+
+set.seed(100)
+# Learn forward error rates
+errF <- learnErrors(filtFs, nbases=1e8, multithread=TRUE)
+# Learn reverse error rates
+errR <- learnErrors(filtRs, nbases=1e8, multithread=TRUE)
+
+# Sample inference and merger of paired-end reads
+mergers <- vector("list", length(sample.names))
+names(mergers) <- sample.names
+
+for(sam in sample.names) {
+  cat("Processing:", sam, "\n")
+  derepF <- derepFastq(filtFs[[sam]])
+  ddF <- dada(derepF, err=errF, multithread=TRUE)
+  derepR <- derepFastq(filtRs[[sam]])
+  ddR <- dada(derepR, err=errR, multithread=TRUE)
+  merger <- mergePairs(ddF, derepF, ddR, derepR)
+  mergers[[sam]] <- merger
+}
+rm(derepF); rm(derepR)
+
+# Construct sequence table and remove chimeras
+seqtab <- makeSequenceTable(mergers)
+saveRDS(seqtab, "E:/tbenhor/libraries/Documents/Oyster-Microbiota-Spring-Mortality/run1/output") # CHANGE ME to where you want sequence table saved
